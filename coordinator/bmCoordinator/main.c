91b97628c8390858861d14d602e467f5c5c43759
Daniel Vilas <daniel.vilas@gmail.com>
2018-05-26 15:54:04 +0000

    new Data

--- coordinator/bmCoordinator/main.c
+++ coordinator/bmCoordinator/main.c
@@ -29,7 +29,7 @@ int execute(pConfig cfg, pPlatform plat, pLang lang, pProtocol proto, int i){
         
     }
     closeRemoteProc(&remoteProc);
-    waitRead(6,&rp_datalogger);
+    waitRead(4,&rp_datalogger);
     closeDataLogger(&rp_datalogger);
     return getRemoteProc(&remoteProc,cfg,plat,lang, proto,i);
 }
